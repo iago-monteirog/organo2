61fc2ce21c479c2237604e01fbff68fe7ed6ae7b
iago-monteirog <iagomgarcia9000@gmail.com>
2022-08-19 16:36:40 -0300

    adiciona cadastro de colaboradores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Formulario from './components/Formulario/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
