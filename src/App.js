d26dd12d325b303ea0908f52524351a17d30cdca
iago-monteirog <iagomgarcia9000@gmail.com>
2022-08-24 20:56:18 -0300

    cria componente Time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Formulario from './components/Formulario/Formulario';
+import Time from './components/Time/Time';
 
 function App() {
 
@@ -13,6 +14,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
+      <Time nome="Data Science"/>
     </div>
   );
 }
